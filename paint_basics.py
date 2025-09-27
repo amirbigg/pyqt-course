21646ad8609df4d36b70efebb44d73f410ea5625
amir <amirbig>
2025-09-27 12:00:06 +0330

    pyqt

---
+++ paint_basics.py
@@ -0,0 +1,151 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtCore import Qt, QPoint, QRect
+from PyQt6.QtGui import QPainter, QPainterPath, QColor, QBrush, QPen, QFont, QPolygon, QLinearGradient
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initializeUI()
+
+    def initializeUI(self):
+        self.setFixedSize(600, 600)
+        self.setWindowTitle("QPainter Basics")
+
+        self.black = '#000000'
+        self.blue = Qt.GlobalColor.blue
+        self.green = Qt.GlobalColor.green
+        self.purple = Qt.GlobalColor.magenta
+        self.red = Qt.GlobalColor.red
+        self.orange = Qt.GlobalColor.yellow
+
+        self.show()
+
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+        self.drawPoints(painter)
+        self.drawDiffLines(painter)
+        self.drawText(painter)
+        self.drawRectangles(painter)
+        self.drawPolygons(painter)
+        self.drawRoundedRects(painter)
+        self.drawCurves(painter)
+        self.drawCircle(painter)
+        self.drawGradient(painter)
+        painter.end()
+
+    def drawPoints(self, painter):
+        pen = QPen(QColor(self.black))
+        for i in range(1, 9):
+            pen.setWidth(i * 2)
+            painter.setPen(pen)
+            painter.drawPoint(i * 20, i * 20)
+
+    def drawDiffLines(self, painter):
+        pen = QPen(QColor(self.black), 2)
+
+        painter.setPen(pen)
+        painter.drawLine(230, 20, 230, 180)
+
+        pen.setStyle(Qt.PenStyle.DashLine)
+        painter.setPen(pen)
+        painter.drawLine(260, 20, 260, 180)
+
+        pen.setStyle(Qt.PenStyle.DotLine)
+        painter.setPen(pen)
+        painter.drawLine(290, 20, 290, 180)
+
+        pen.setStyle(Qt.PenStyle.DashDotLine)
+        painter.setPen(pen)
+        painter.drawLine(320, 20, 320, 180)
+
+        blue_pen = QPen(QColor(self.blue), 4)
+        blue_pen.setStyle(Qt.PenStyle.DashDotDotLine)
+
+        painter.setPen(blue_pen)
+        painter.drawLine(350, 20, 350, 180)
+
+    def drawText(self, painter):
+        pen = QPen(QColor(self.red))
+        painter.setFont(QFont("Helvetica", 15))
+        painter.setPen(pen)
+        painter.drawText(420, 110, "Hello user..!")
+
+    def drawRectangles(self, painter):
+        pen = QPen(QColor(self.black))
+        brush = QBrush(QColor(self.black))
+
+        painter.setPen(pen)
+        painter.drawRect(20, 220, 80, 80)
+
+        painter.setPen(pen)
+        painter.setBrush(brush)
+        painter.drawRect(120, 220, 80, 80)
+
+        red_pen = QPen(QColor(self.red), 5)
+        green_brush = QBrush(QColor(self.green))
+
+        painter.setPen(red_pen)
+        painter.setBrush(green_brush)
+        painter.drawRect(20, 320, 80, 80)
+
+    def drawPolygons(self, painter):
+        pen = QPen(QColor(self.blue), 2)
+        brush = QBrush(QColor(self.orange))
+        points = QPolygon([QPoint(240, 240), QPoint(380, 250), QPoint(230, 380), QPoint(370, 360)])
+        painter.setPen(pen)
+        painter.setBrush(brush)
+        painter.drawPolygon(points)
+
+    def drawRoundedRects(self, painter):
+        pen = QPen(QColor(self.black))
+        brush = QBrush(QColor(self.black))
+
+        rect_1 = QRect(420, 340, 40, 60)
+
+        painter.setPen(pen)
+        brush.setStyle(Qt.BrushStyle.Dense5Pattern)
+        painter.setBrush(brush)
+        painter.drawRoundedRect(rect_1, 20, 20)
+
+    def drawCurves(self, painter):
+        pen = QPen(QColor(self.black), 3)
+        brush = QBrush(Qt.GlobalColor.white)
+
+        tail_path = QPainterPath()
+        tail_path.moveTo(30, 420)
+        tail_path.cubicTo(30, 420, 65, 500, 30, 560)
+        tail_path.lineTo(163, 540)
+        tail_path.cubicTo(125, 360, 110, 440, 30, 420)
+        tail_path.closeSubpath()
+
+        painter.setPen(pen)
+        painter.setBrush(brush)
+        painter.drawPath(tail_path)
+
+    def drawCircle(self, painter):
+        pen = QPen(QColor(self.black), 2, Qt.PenStyle.SolidLine)
+        brush = QBrush(Qt.GlobalColor.darkMagenta, Qt.BrushStyle.Dense2Pattern)
+
+        painter.setPen(pen)
+        painter.setBrush(brush)
+        painter.drawEllipse(QPoint(300, 500), 60, 60)
+
+    def drawGradient(self, painter):
+        pen = QPen(QColor(self.black), 2)
+        gradient = QLinearGradient(450, 480, 520, 550)
+        gradient.setColorAt(0.0, Qt.GlobalColor.blue)
+        gradient.setColorAt(0.5, Qt.GlobalColor.yellow)
+        gradient.setColorAt(1.0, Qt.GlobalColor.cyan)
+        painter.setPen(pen)
+        painter.setBrush(QBrush(gradient))
+        painter.drawRect(420, 420, 160, 160)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
